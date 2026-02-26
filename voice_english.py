2bb9310ba081a25f027eab8ffd2f6e4afb38c5f7
vinoth-04 <vinoth17170@gmail.com>
2026-02-26 21:00:03 +0530

    add new files

---
+++ voice_english.py
@@ -0,0 +1,130 @@
+import os
+from dotenv import load_dotenv
+from loguru import logger
+
+from pipecat.audio.vad.silero import SileroVADAnalyzer
+from pipecat.frames.frames import LLMRunFrame
+from pipecat.pipeline.pipeline import Pipeline
+from pipecat.pipeline.runner import PipelineRunner
+from pipecat.pipeline.task import PipelineParams, PipelineTask
+from pipecat.processors.aggregators.llm_context import LLMContext
+from pipecat.processors.aggregators.llm_response_universal import (
+    LLMContextAggregatorPair,
+    LLMUserAggregatorParams,
+)
+
+from pipecat.services.deepgram.stt import DeepgramSTTService
+from pipecat.services.sarvam import SarvamTTSService
+from pipecat.transcriptions.language import Language
+
+from pipeline.services.openai.llm import OpenAILLMService
+from prompt.clinic_system_prompt import SYSTEM_PROMPT
+from pipecat.adapters.schemas.tools_schema import ToolsSchema
+
+# ⭐ IMPORT YOUR TOOL FUNCTIONS
+from database.tools import check_slot, book_slot
+
+load_dotenv()
+
+
+async def run_bot(transport):
+
+    # ================= LLM =================
+    llm = OpenAILLMService(
+        api_key=os.getenv("OPENAI_API_KEY"),
+        model="gpt-4o-mini",
+        params=OpenAILLMService.InputParams(
+            temperature=0.4,
+            max_completion_tokens=200,
+            frequency_penalty=0.3,
+        ),
+        retry_on_timeout=True,
+    )
+    # ================= TOOLS =================
+    tools_schema = ToolsSchema(
+        standard_tools=[check_slot, book_slot]
+    )
+
+    # 1) Give schema to LLM
+    llm.tools = tools_schema
+
+    # 2) Register actual handler functions
+    llm.register_direct_function(check_slot)
+    llm.register_direct_function(book_slot)
+    # # ================= TOOLS =================
+    # tools = ToolsSchema(
+    #     standard_tools=[check_slot, book_slot]
+    # )
+
+    # # ⭐ REGISTER TOOLS IN LLM
+    # llm.tools = tools
+
+    # ================= STT =================
+    stt = DeepgramSTTService(
+        api_key=os.getenv("DEEPGRAM_API_KEY"),
+    )
+
+    # ================= TTS =================
+    tts = SarvamTTSService(
+        api_key=os.getenv("SARVAM_API_KEY"),
+        model="bulbul:v3-beta",
+        voice_id="shubh",
+        params=SarvamTTSService.InputParams(
+            language=Language.EN,
+            pace=1.1,
+            temperature=0.6,
+        ),
+    )
+
+    # ================= CONTEXT =================
+    messages = [{"role": "system", "content": SYSTEM_PROMPT}]
+
+    context = LLMContext(
+        messages=messages,
+        tools=tools_schema   # ⭐ MUST BE ToolsSchema
+    )
+
+    user_agg, assistant_agg = LLMContextAggregatorPair(
+        context,
+        user_params=LLMUserAggregatorParams(
+            vad_analyzer=SileroVADAnalyzer(),
+        ),
+    )
+
+    # ================= PIPELINE =================
+    pipeline = Pipeline(
+        [
+            transport.input(),
+            stt,
+            user_agg,
+            llm,
+            tts,
+            transport.output(),
+            assistant_agg,
+        ]
+    )
+
+    task = PipelineTask(
+        pipeline,
+        params=PipelineParams(
+            audio_in_sample_rate=8000,
+            audio_out_sample_rate=8000,
+        ),
+    )
+
+    # ================= EVENTS =================
+    @transport.event_handler("on_client_connected")
+    async def on_connected(transport, client):
+        logger.info("📞 Caller connected")
+        # messages.append(
+        #     {"role": "system", "content": "Please introduce yourself to the user."}
+        # )
+        await task.queue_frames([LLMRunFrame()])
+
+    @transport.event_handler("on_client_disconnected")
+    async def on_disconnected(transport, client):
+        logger.info("📴 Caller disconnected")
+        await task.cancel()
+
+    runner = PipelineRunner(handle_sigint=False)
+    await runner.run(task)

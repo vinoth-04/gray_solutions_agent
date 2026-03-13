9da96b6b32b62d58e44e69d0274eeb5ab8771dbd
vinoth-04 <vinoth17170@gmail.com>
2026-03-13 21:00:42 +0530

    RDS

--- voice_english.py
+++ voice_english.py
@@ -23,13 +23,11 @@ from pipecat.adapters.schemas.tools_schema import ToolsSchema
 
 from database.time_utils import get_current_context 
 from database.tools import check_slot, book_slot, next_available_slot, cancel_slot, reschedule_slot
-from Conversation_State.intent import ConversationStateManager
 
 load_dotenv()
 
-state_manager = ConversationStateManager()
 
-async def run_bot(transport, call_id: str):
+async def run_bot(transport):
 
     # ================= LLM =================
     llm = OpenAILLMService(
@@ -48,7 +46,7 @@ async def run_bot(transport, call_id: str):
             book_slot,
             next_available_slot,
             cancel_slot,
-            reschedule_slot,
+            reschedule_slot
         ]
     )
     # time_context = get_current_context()
@@ -59,7 +57,6 @@ async def run_bot(transport, call_id: str):
     llm.register_direct_function(next_available_slot)
     llm.register_direct_function(cancel_slot)
     llm.register_direct_function(reschedule_slot)
-
         # ================= STT =================
     stt = DeepgramSTTService(
         api_key=os.getenv("DEEPGRAM_API_KEY"),
@@ -124,22 +121,16 @@ async def run_bot(transport, call_id: str):
     # ================= EVENTS =================
     @transport.event_handler("on_client_connected")
     async def on_connected(transport, client):
-
-        logger.info(f"📞 Caller connected: {call_id}")
-
-        state_manager.create_session(call_id)
-
+        logger.info("📞 Caller connected")
+        # messages.append(
+        #     {"role": "system", "content": "Please introduce yourself to the user."}
+        # )
         await task.queue_frames([LLMRunFrame()])
 
     @transport.event_handler("on_client_disconnected")
     async def on_disconnected(transport, client):
-
-        logger.info(f"📴 Caller disconnected: {call_id}")
-
-        state_manager.delete_session(call_id)
-
+        logger.info("📴 Caller disconnected")
         await task.cancel()
 
-    # ================= RUN =================
-    runner = PipelineRunner()
+    runner = PipelineRunner(handle_sigint=False)
     await runner.run(task)
